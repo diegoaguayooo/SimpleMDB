6c347d5bd5cb897c9e66cd8bada13501daef5751
diego aguayo ⚡︎ <diegoaguayorivera@diegos-macbook-air-2.local>
2025-09-22 19:01:28 -0400

    Initial commit

---
+++ src/App.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMDB
+{
+    public class App
+    {
+        private HttpListener server;
+
+        public App()
+        {
+            string host = "http://127.0.0.1:8080/";
+            server = new HttpListener();
+            server.Prefixes.Add(host);
+
+            Console.WriteLine("Server listening on... " + host);
+        }
+
+        public async Task Start()
+        {
+            server.Start();
+
+            while (server.IsListening)
+            {
+                var ctx = await server.GetContextAsync(); // async version is better
+                await HandleContextAsync(ctx);
+            }
+        }
+
+        public void Stop()
+        {
+            server.Stop();
+            server.Close();
+        }
+
+        private async Task HandleContextAsync(HttpListenerContext ctx)
+        {
+            var req = ctx.Request;
+            var res = ctx.Response;
+
+            if (req.HttpMethod.Equals("GET", StringComparison.OrdinalIgnoreCase) &&
+                req.Url!.AbsolutePath == "/")
+            {
+                string html = "Hello!";
+                byte[] content = Encoding.UTF8.GetBytes(html);
+
+                res.StatusCode = (int)HttpStatusCode.OK;
+                res.ContentEncoding = Encoding.UTF8;
+                res.ContentType = "text/plain";
+                res.ContentLength64 = content.LongLength;
+                await res.OutputStream.WriteAsync(content);
+                res.Close();
+            }
+        }
+    }
+}
